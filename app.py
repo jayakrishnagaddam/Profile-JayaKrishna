04ed9d9f79be986f324e664351fcb7ac83eb9fc7
Jaya Krishna Gaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2024-02-08 23:53:11 +0530

    Added weekend endpoint

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def function():
   return render_template('mainpage.html')
@@ -11,6 +10,10 @@ def function():
 @app.route('/contact')
 def contact():
   return render_template('contact.html')
+
+@app.route('/weekend')
+def weekend():
+  return render_template('weekend.html')
 
 
 if __name__ == '__main__':
